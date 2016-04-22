19319c979dbeeedb3593ad9e7c8392550e242171
aquila62 <orvalbounds@yahoo.com>
2016-04-22 07:08:57 -0500

    Added the Mersenne Twister for comparison

--- mcpi.c
+++ mcpi.c
@@ -40,6 +40,7 @@ int main(void)
    {
    int i;                     /* loop counter                 */
    int etauselap;             /* elapsed time for etaus       */
+   int mtelap;                /* elapsed time for mt19937     */
    int ranlxelap;             /* elapsed time for ranlxd2     */
    unsigned int dttk;         /* combined date, time #ticks   */
    double tot;                /* total points within a circle */
@@ -49,6 +50,8 @@ int main(void)
    clock_t clk;               /* current number of ticks      */
    clock_t etausstart;        /* start time for etaus         */
    clock_t etausfin;          /* end   time for etaus         */
+   clock_t mtstart;           /* start time for mt19937       */
+   clock_t mtfin;             /* end   time for mt19937       */
    clock_t ranlxstart;        /* start time for ranlxd2       */
    clock_t ranlxfin;          /* end   time for ranlxd2       */
    struct tms t;              /* structure used by times()    */
@@ -74,7 +77,43 @@ int main(void)
    ratio = tot / bot;         /* calculate est. pi/4   */
    etausfin = times(&t);      /* finish time for etaus */
    printf("Monte Carlo Calculation of Pi/4\n");
+   printf("      n = 100 million\n");
    printf("      etaus Pi/4 %18.15f\n", ratio);
+   /************************************************************/
+   /* allocate the mt19937 random number generator */
+   r = (gsl_rng *) gsl_rng_alloc(gsl_rng_mt19937);
+   /* get clock ticks since boot                           */
+   clk = times(&t);
+   /* get date & time                                      */
+   time(&now);
+   /* combine date, time, and ticks into a single UINT     */
+   dttk = (unsigned int) (now ^ clk);
+   /* initialize the GSL ranlxd2 random number generator      */
+   /* to date,time,#ticks                                     */
+   gsl_rng_set(r, dttk);      /* initialize ranlxd2 seed      */
+   tot = 0.0;                 /* initialize total points in a circle */
+   i = (int) bot;             /* set loop counter */
+   /* get clock ticks since boot                              */
+   mtstart = times(&t);       /* start time for GSL mt19937   */
+   while (i--)                /* loop 100 million times       */
+      {
+      double x;               /* horizontal value             */
+      double y;               /* vertical   value             */
+      double sum;             /* sum = x*x + y*y              */
+      /* use the mt19937 random number generator this time    */
+      x = gsl_rng_uniform(r);      /* uniform number 0-1      */
+      y = gsl_rng_uniform(r);      /* uniform number 0-1      */
+      sum = (x*x) + (y*y);    /* calculate the vector length  */
+      if (sum < 1.0) tot += 1.0;   /* if within a unit circle, tally */
+      } /* for each point inside or outside a circle */
+   ratio = tot / bot;         /* calculate est. pi/4           */
+   mtfin = times(&t);         /* finish time for GSL mt19937   */
+   printf("GSL mt19937 Pi/4 %18.15f\n", ratio);
+   printf("     Actual Pi/4 %18.15f\n", M_PI * 0.25);
+   etauselap = etausfin - etausstart;
+   mtelap    = mtfin    - mtstart;
+   printf("      etaus ticks %6d\n", etauselap);
+   printf("GSL mt19937 ticks %6d\n", mtelap);
    /************************************************************/
    /* allocate the ranlxd2 random number generator */
    r = (gsl_rng *) gsl_rng_alloc(gsl_rng_ranlxd2);
